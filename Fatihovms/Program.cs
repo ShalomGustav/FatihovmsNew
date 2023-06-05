a97c0bad2eeb915bc35ce4fe89b281a9c9122bf9
ShalomGustav <maximvaryagov@gmail.com>
2023-06-05 14:47:09 +0300

    Первый метод

--- Fatihovms/Program.cs
+++ Fatihovms/Program.cs
@@ -10,3 +10,26 @@ void array(string[] stringArray)//метод заполнения
         stringArray[i] = Console.ReadLine();
     }
 }
+string[] symbol(string[] stringArray)
+{
+    int n = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            n++;
+        }
+
+    }
+    string[] rez = new string[n];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            rez[j] = stringArray[i];
+            j++;
+        }
+    }
+    return rez;
+}
