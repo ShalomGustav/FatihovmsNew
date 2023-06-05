d0b963dcfcc9159671c4c7e9fb8b27c62a877fb9
ShalomGustav <maximvaryagov@gmail.com>
2023-06-05 14:46:42 +0300

    Добавьте файлы проекта.

---
+++ Fatihovms/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите количество элементов массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[m];
+void array(string[] stringArray)//метод заполнения
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        stringArray[i] = Console.ReadLine();
+    }
+}
